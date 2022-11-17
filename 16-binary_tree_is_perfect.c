d709593e3c4eebd79e5258ebd13c7132e9a3c6f8
Darren <darren2097@gmail.com>
2022-11-17 17:47:42 +0200

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,30 +25,17 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t depth = binary_tree_depth(tree);
+	int level = 0;
 
-	return (is_perfect(tree, depth, 0));
-}
-
-/**
- * is_perfect - Checks if tree is perfect
- * @tree: pointer to the root node of the tree to check
- * @depth: depth of tree
- * @level: level on tree
- *
- * Return: 1 if it is perfect tree, else 0 if tree is NULL or not perfect
- */
-
-int is_perfect(const binary_tree_t *tree, size_t depth, int level)
-{
 	if (tree == NULL)
 		return (0);
-
+	
 	if (tree->left == NULL && tree->right == NULL)
 		return (depth = level + 1);
 
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	return (is_perfect(tree->left, depth, level + 1) &&
-			is_perfect(tree->right, depth, level + 1));
+	return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
 }
