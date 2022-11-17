042fac0eda501f11d7f5f15dc0bbfc59c2474955
Darren <darren2097@gmail.com>
2022-11-17 14:30:12 +0200

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -5,7 +5,7 @@
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node.
  * The value in the node must be passed as a parameter to this function
- * 
+ *
  * Return: void
  */
 
