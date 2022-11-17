933616222e637fda2cb6d19d08f5ee85d178bbd9
Darren <darren2097@gmail.com>
2022-11-17 15:18:47 +0200

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,15 +9,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
+	if (tree != NULL)
 	{
 		size_t left_height = binary_tree_height(tree->left);
 		size_t right_height = binary_tree_height(tree->right);
 
-		return (MAX(left_height, right_height) + 1);
+		return ((left_height, right_height) + 1);
 	}
+
+	return (0);
 }
